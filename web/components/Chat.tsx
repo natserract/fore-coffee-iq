f10de38f916add869481dd2565a2208bdc88e444
natserract <benjaminstwo@gmail.com>
2024-10-03 20:44:03 +0800

    ui: rendering position

--- web/components/Chat.tsx
+++ web/components/Chat.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import ChatArea from "~/components/ChatArea";
+import QA from "~/components/QA";
 import { ChatProvider } from "~/lib/context/ChatProvider/ChatProvider";
 
 const Chat = () => {
   return (
     <ChatProvider>
-      <ChatArea />
+      <QA />
     </ChatProvider>
   );
 };
