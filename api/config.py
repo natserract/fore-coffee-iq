350d785d4ecbb3932c82bd2c1b3d375fc0383896
natserract <benjaminstwo@gmail.com>
2024-10-04 11:55:09 +0800

    remove unused modules

--- api/config.py
+++ api/config.py
@@ -1,7 +1,5 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
 PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
 PINECONE_INDEX_HOST = os.environ.get("PINECONE_INDEX_HOST") or ''
